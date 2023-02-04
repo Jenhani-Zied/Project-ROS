271319ae3415b46faf82274e8fca78859008c5bd
Jenhani-Zied <zied.jenhani@etudiant-enit.utm.tn>
2023-02-04 20:06:16 +0100

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,11 @@
+# CMake generated Testfile for 
+# Source directory: /home/s111p03/catkin_ws/src
+# Build directory: /home/s111p03/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("mobile_robot_simulation")
+subdirs("mobile_robot_base")
+subdirs("mobile_robot_description")
+subdirs("mobile_robot")
